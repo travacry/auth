c202cc3a469fb3287b4b2a470b6d9b80823af997
Alex K @ travacry <prog.alex.k@gmail.com>
2024-07-17 21:32:52 +0300

    - fix return obj, err

--- cmd/grpc_client/main.go
+++ cmd/grpc_client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
 	"golang.org/x/net/context"
@@ -37,14 +38,34 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+	// log.Printf("failed to get user by id: %v", err)
+	_, err = createUser(ctx, client)
 
-	createUser(ctx, client)
-	getUser(ctx, client)
-	updateUser(ctx, client)
-	deleteUser(ctx, client)
+	if err != nil {
+		log.Print(err)
+	}
+
+	_, err = getUser(ctx, client)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = updateUser(ctx, client)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = deleteUser(ctx, client)
+
+	if err != nil {
+		log.Print(err)
+	}
 }
 
-func createUser(ctx context.Context, client desc.UserV1Client) {
+func createUser(ctx context.Context, client desc.UserV1Client) (*desc.CreateResponse, error) {
+
 	createRequest, err := client.Create(ctx, &desc.CreateRequest{
 		Info: &desc.UserInfo{
 			Name:  gofakeit.Name(),
@@ -56,22 +77,26 @@ func createUser(ctx context.Context, client desc.UserV1Client) {
 	})
 
 	if err != nil {
-		log.Printf("failed to get user by id: %v", err)
-		return
+		return nil, fmt.Errorf("failed to create user: %v", err)
 	}
+
 	log.Printf(color.RedString("Create user:\n"), color.GreenString("%+d", createRequest.GetId()))
+	return createRequest, nil
 }
 
-func getUser(ctx context.Context, client desc.UserV1Client) {
+func getUser(ctx context.Context, client desc.UserV1Client) (*desc.GetResponse, error) {
+
 	getRequest, err := client.Get(ctx, &desc.GetRequest{Id: userID})
+
 	if err != nil {
-		log.Printf("failed to get user by id: %v", err)
-		return
+		return nil, fmt.Errorf("failed to get user by id: %v", err)
 	}
+
 	log.Printf(color.RedString("Get user:\n"), color.GreenString("%+v", getRequest.GetUser()))
+	return getRequest, nil
 }
 
-func updateUser(ctx context.Context, client desc.UserV1Client) {
+func updateUser(ctx context.Context, client desc.UserV1Client) error {
 	_, err := client.Update(ctx, &desc.UpdateRequest{
 		Info: &desc.UpdateUserInfo{
 			Id:    userID,
@@ -80,20 +105,24 @@ func updateUser(ctx context.Context, client desc.UserV1Client) {
 			Role:  desc.Role_user,
 		},
 	})
+
 	if err != nil {
-		log.Printf("failed to update user: %v", err)
-		return
+		return fmt.Errorf("failed to update user: %v", err)
 	}
+
 	log.Print(color.RedString("Update user.\n"))
+	return nil
 }
 
-func deleteUser(ctx context.Context, client desc.UserV1Client) {
+func deleteUser(ctx context.Context, client desc.UserV1Client) error {
 	_, err := client.Delete(ctx, &desc.DeleteRequest{
 		Id: userID,
 	})
+
 	if err != nil {
-		log.Printf("failed to delete user: %v", err)
-		return
+		return fmt.Errorf("failed to delete user: %v", err)
 	}
+
 	log.Print(color.RedString("Delete user.\n"))
+	return nil
 }
